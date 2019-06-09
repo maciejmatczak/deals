527a7d0429e599207ff2eaeabe32f58b376bc5e1
Maciej Matczak <maciek.matczak@gmail.com>
2019-06-09 16:17:19 +0200

    Add absolute_url and description field

--- scraper/models.py
+++ scraper/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 
 User = get_user_model()
@@ -7,12 +8,19 @@ User = get_user_model()
 
 class ScrapingTask(models.Model):
     url = models.URLField(max_length=600, blank=False)
-    multiple = models.BooleanField(default=False, blank=False)
     active = models.BooleanField(default=True, blank=False)
+    multiple = models.BooleanField(default=False, blank=False)
     task = models.TextField(blank=False)
+    description = models.TextField(blank=True)
     running_time = models.TimeField(blank=False)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.url
+
+    def get_absolute_url(self):
+        return reverse('scrapingtask-detail', kwargs={'pk': self.pk})
 
 
 class Product(models.Model):
