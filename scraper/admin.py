3dcb53842ffb5a30d5884461e4a0e2146b1a375e
Maciej Matczak <maciek.matczak@gmail.com>
2019-07-02 21:15:45 +0200

    Add yaml validator and initially new Item model

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import ScrapingTask, Product
+from .models import ScrapingTask, Product, Item
 
 admin.site.register(ScrapingTask)
 admin.site.register(Product)
+admin.site.register(Item)
--- scraper/models.py
+++ scraper/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.urls import reverse
+import yaml
 
 
 User = get_user_model()
+
+
+def validate_yaml(value):
+    try:
+        yaml.safe_load(value)
+    except yaml.YAMLError as exception:
+        raise ValidationError('Field is not a proper yaml: %(exception)s',
+                              params={'exception': exception}
+                              )
 
 
 class ScrapingTask(models.Model):
     url = models.URLField(max_length=600, blank=False)
     active = models.BooleanField(default=True, blank=False)
     multiple = models.BooleanField(default=False, blank=False)
-    task = models.TextField(blank=False)
+    task = models.TextField(blank=False, validators=[validate_yaml])
     description = models.TextField(blank=True)
     running_time = models.TimeField(blank=False)
 
@@ -34,3 +45,8 @@ class Product(models.Model):
 
     scraping_task = models.ForeignKey(ScrapingTask, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Item(models.Model):
+    data = models.TextField(blank=False, validators=[validate_yaml])
+    scraping_task = models.ForeignKey(ScrapingTask, on_delete=models.SET_NULL, null=True)
