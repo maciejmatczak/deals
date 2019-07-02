483ed500bcc8dc86bca4d7567f4462099627f0e5
Maciej Matczak <maciek.matczak@gmail.com>
2019-07-02 21:15:17 +0200

    Add common setting for scraper's chromedriver path

--- deals/settings/common.py
+++ deals/settings/common.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from os import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -117,3 +118,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'scraper-home'
 LOGIN_URL = 'login'
+
+SCRAPER_CHROMEDRIVER_PATH = environ['DJANGO_SCRAPER_CHROMEDRIVER_PATH']
