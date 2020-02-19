b9c8618d36200cc16ce28bb644ed49855ef7347a
Maciej Matczak <maciek.matczak@gmail.com>
2020-02-19 20:59:12 +0100

    All local ips for allowed_hosts

--- deals/settings/local.py
+++ deals/settings/local.py
@@ -3,7 +3,7 @@ from .common import *
 
 SECRET_KEY = 'secret!'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 ADMIN_URL = 'admin/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
